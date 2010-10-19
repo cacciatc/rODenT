1d5bcead2dd0aa8fa587796fe51bab0da18cae8e
= <=>
2010-10-18 22:01:18 -0700

    .

--- lib/rODenT.rb
+++ lib/rODenT.rb
@@ -6,7 +6,6 @@ class Whiskers
   include Nokogiri, Zip
   
   attr_accessor :file_string
-  
   def initialize(file_name)
     odt = ZipFile.open(file_name)
     @content_xml = XML::parse(odt.read('content.xml'))
