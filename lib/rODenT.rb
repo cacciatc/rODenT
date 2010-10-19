00545a6c4fc5f5a00c3f783d80fd3c1d9f58abbb
= <=>
2010-10-18 21:51:35 -0700

    added basic func to get lists and paras

--- lib/rODenT.rb
+++ lib/rODenT.rb
@@ -1,28 +1,39 @@
+require 'rubygems'
+require 'nokogiri'
+require 'zip/zipfilesystem'
+
 class Whiskers
+  include Nokogiri, Zip
   attr_accessor :file_string
-  def initialize(file_string)
-    @file_string = file_string
+  def initialize(file_name)
+    odt = ZipFile.open(file_name)
+    @content_xml = XML::parse(odt.read('content.xml'))
   end
   def self.scurry(string,&b)
     yield Whiskers.new(string)
   end
   def paragraphs
-    Whiskers.paragraphs(@file_string)
+    Whiskers.paragraphs(@content_xml)
   end
   def lists
-    Whiskers.lists(@file_string)
+    Whiskers.lists(@content_xml)
   end
   def footnotes
-    Whiskers.footnotes(@file_string)
+    Whiskers.footnotes(@content_xml)
   end
-  def self.paragraphs(string)
-    
+  def self.paragraphs(xml)
+    xml.xpath('office:document-content/office:body/office:text/text:p')
   end
-  def self.lists(string)
-    
+  def self.lists(xml)
+    xml.xpath('office:document-content/office:body/office:text/text:list')
   end
-  def self.footnotes(string)
+  def self.footnotes(xml)
     
   end
   private :initialize
+end
+
+Whiskers.scurry 'test/test.odt' do |rat|
+  puts rat.lists
+  puts rat.paragraphs
 end
