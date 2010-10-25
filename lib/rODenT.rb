bf084b421811c1be4435c8fe58fce908d9946f18
cacciatc <chris.cacciatore@gmail.com>
2010-10-24 17:43:20 -0700

    added support for italics, underline, and bold.

--- lib/rODenT.rb
+++ lib/rODenT.rb
@@ -27,29 +27,38 @@ class Rodent
   attr_accessor :file_string
   
   OR    = '|'
+  TAB   = 'text:tab'
   PARA  = 'text:p'
+  SPAN  = 'text:span'
   
   LIST      = 'text:list'
   LIST_ITEM = 'text:list-item/text:p'
   
-  TABLE = 'table:table'
+  TABLE      = 'table:table'
   TABLE_ROW  = 'table:table-row'
   TABLE_COL  = 'table:table-col'
   TABLE_CELL = 'table:table-cell'
+  
+  DOC_STYLES = 'office:document-content/office:automatic-styles/style:style'
   
   DOC   = 'office:document-content/office:body/office:text/'
   STYLE = 'office:document-styles/office:master-styles/style:master-page/style:footer'
   
   def initialize(file_name)
     odt = ZipFile.open(file_name)
-    @content_xml = XML::parse(odt.read('content.xml'))
-    @styles_xml   = XML::parse(odt.read('styles.xml'))
+    content_xml = XML::parse(odt.read('content.xml'))
+    styles_xml  = XML::parse(odt.read('styles.xml'))
+    
+    resolved_styles = preprocess_styles(content_xml)
     @lists,@paras,@tables,@footnotes = [],[],[],[]
+    
     #inject was giving funny behavior...so looping explicitly
     i = 1
-    @content_xml.xpath("#{DOC} #{PARA} #{OR} 
-                        #{DOC} #{LIST} #{OR}
-                        #{DOC} #{TABLE}").to_a.each do |node|
+    content_xml.xpath("#{DOC} #{PARA} #{OR} 
+                       #{DOC} #{LIST} #{OR}
+                       #{DOC} #{TABLE}").each do |node|
+      
+      node = substitute_styles(node,resolved_styles)
       #got to be a better way to do this...
       case node.to_s
         when /^<text:p/
@@ -65,7 +74,7 @@ class Rodent
         end
         i += 1
       end
-    @styles_xml.xpath(STYLE).each do |node|
+    styles_xml.xpath(STYLE).each do |node|
       @footnotes << {:node=>node.text}
     end
   end
@@ -91,8 +100,46 @@ class Rodent
   def tables(&b)
     Rodent.guarded(@tables,&b)
   end
-  private :initialize
+  def preprocess_styles(content_xml)
+    styles = {}
+    content_xml.xpath(DOC_STYLES).each do |node|
+      if node.attributes['family'].text == "text"
+        styles[node.attributes['name'].text] = []
+        node.xpath('style:text-properties').each do |n|
+          if n.attributes['font-style']
+            styles[node.attributes['name'].text] << %q{"<i>#{content}</i>"}
+          end
+          if n.attributes['font-weight']
+            styles[node.attributes['name'].text] << %q{"<b>#{content}</b>"}
+          end
+          if n.attributes['text-underline-style']
+            styles[node.attributes['name'].text] << %q{"<u>#{content}</u>"}
+          end
+        end
+      end
+    end
+    styles
+  end
+  #also does whitespace like tabs
+  def substitute_styles(node,styles)
+    node.xpath("#{TAB} #{OR} #{SPAN}").each do |n|
+      case n.to_s
+        when /^<text:tab/
+          n.content = "     "
+        when /^<text:span/
+          styles[n.attributes['style-name'].text].each do |s|
+            content = n.content
+            n.content = eval(s)
+          end
+      end
+    end
+    node
+  end
+  private :initialize, :preprocess_styles, :substitute_styles
 end
 class Rat < Rodent;end
 class Mouse < Rodent;end
 class SmallFurryThing < Rodent;end
+
+rat = Rat.scurry('test/test.odt')
+puts rat.paragraphs
